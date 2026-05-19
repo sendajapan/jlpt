cf99a67e51e7df6fdb331a18de64c18069d68d1b
Muhammad Nouman <mnoman55@gmail.com>
2026-05-19 14:50:25 +0900

    vocab page re-design

--- resources/views/admin/vocab/words/_audio_cell.blade.php
+++ resources/views/admin/vocab/words/_audio_cell.blade.php
@@ -36,7 +36,7 @@
     @endif
 
     <template x-if="!gone && !url && {{ $existing ? 'true' : 'false' }}">
-        <div id="id_{{$field}}" class="inline-flex items-center gap-1 flex-wrap">
+        <div id="id_{{$field}}" class="inline-flex items-center gap-1">
             <button class="inline-flex h-8 items-center gap-1.5 px-3 rounded-md border border-blue-200 bg-white hover:bg-blue-50 text-xs font-medium text-blue-600 transition-colors" onclick="document.getElementById('{{ $audioId }}').play()">
                 <svg class="w-3.5 h-3.5" fill="currentColor" viewBox="0 0 20 20"><path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z"/></svg>
                 Play
