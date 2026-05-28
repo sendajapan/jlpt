5fb21fb8e024e0ec14fb1911e0d379fe4691982d
smensulaiman <smensulaiman007@gmail.com>
2026-05-28 13:53:22 +0900

    pref: brush up

--- app/Services/VocabularyService.php
+++ app/Services/VocabularyService.php
@@ -30,7 +30,7 @@ class VocabularyService
             ->when(isset($filters['audio_flag']) && $filters['audio_flag'] === 'complete', fn ($q) => $this->applyCompleteAudioFilter($q))
             ->orderBy('sort_order')
             ->orderBy('word_jp')
-            ->tap(fn($q) => dd($q->toRawSql()))
+//            ->tap(fn($q) => dd($q->toRawSql()))
             ->paginate($perPage)
             ->withQueryString();
     }
