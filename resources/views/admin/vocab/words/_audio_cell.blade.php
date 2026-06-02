7f1b5b30db2634ab883f6e9e6f9e866f2c897174
Muhammad Nouman <mnoman55@gmail.com>
2026-06-02 11:58:49 +0900

    status audio check in listing page

--- resources/views/admin/vocab/words/_audio_cell.blade.php
+++ resources/views/admin/vocab/words/_audio_cell.blade.php
@@ -8,10 +8,8 @@
     $toggleReviewedUrl = route('admin.vocab.words.toggle-reviewed', $vocab);
     $initialVoiceId = $vocab->voice_id ?? $defaultVoiceId;
     $initialReviewed = (bool) $vocab->{$reviewedField};
-    $audio_name =$existing;
-
-    dd($existing);
-    //end(explode('/', url()));
+    $audio_name = $existing;
+    $audio = explode($audio_name);
 @endphp
 
 <div x-data="{
@@ -61,7 +59,7 @@
                 <svg class="w-3.5 h-3.5" fill="currentColor" viewBox="0 0 20 20"><path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z"/></svg>
                 Play
             </button>
-            <button @click="window.open('https://pathlingo.scholarlyapps.com/audio_trim.php?audio={{ $audio_name }}', '_blank')" class="inline-flex h-7 items-center gap-1 px-2 rounded border border-green-200 bg-white hover:bg-blue-50 text-[10px] font-medium text-green-600 transition-colors" >
+            <button @click="window.open('https://pathlingo.scholarlyapps.com/audio_trim.php?audio={{ $audio }}', '_blank')" class="inline-flex h-7 items-center gap-1 px-2 rounded border border-green-200 bg-white hover:bg-blue-50 text-[10px] font-medium text-green-600 transition-colors" >
                 Cut
             </button>
 
